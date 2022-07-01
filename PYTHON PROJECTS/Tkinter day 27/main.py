f5c279a3173b0f23308fdb777640e5bcc3fecb50
Emil Terhoven <79977453+eterhoven@users.noreply.github.com>
2022-07-01 15:32:56 +0200

    More playing around

--- PYTHON PROJECTS/Tkinter day 27/main.py
+++ PYTHON PROJECTS/Tkinter day 27/main.py
@@ -1,24 +1,30 @@
-from re import T
-import tkinter
+from tkinter import *
 import turtle
 
 #window
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI program")
 window.minsize(width=500, height=300)
 
 #Label
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
 
-tim = turtle.Turtle()
-tim.write("Some text", font=("Time New Roman", 80, "bold"))
+#button
 
+def button_click():
+    my_label.config(text=input.get())
 
+button = Button(text="Click me", command=button_click)
+button.pack()
 
+#Entry
 
+input = Entry(width=10)
+input.pack()
+input.get()
 
 window.mainloop()
 
