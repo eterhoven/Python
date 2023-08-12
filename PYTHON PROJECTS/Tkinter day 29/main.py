6838e07f5514ce3ab4d5127c334532c41ce7b39c
jumo-writer-github <79977453+eterhoven@users.noreply.github.com>
2023-08-12 13:45:16 +0200

    Add save functionaility

--- PYTHON PROJECTS/Tkinter day 29/main.py
+++ PYTHON PROJECTS/Tkinter day 29/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- CONSTANTS ------------------------------- #
 YELLOW = "#f7f5dd"
@@ -6,7 +7,25 @@ FONT_NAME = "Courier"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+    
+        is_ok =messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,11 +41,13 @@ website_label = Label(text="Website:", font=(FONT_NAME, 15))
 website_label.grid(column=0, row=1)
 website_entry = Entry(window, width=36)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_label = Label(text="Email/Username:", font=(FONT_NAME, 15))
 email_label.grid(column=0, row=2)
 email_entry = Entry(width=36)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "eterhoven@gmail.com")
 
 password_label = Label(text="Password:", font=(FONT_NAME, 15))
 password_label.grid(column=0, row=3)
@@ -36,7 +57,7 @@ password_entry.grid(column=1, row=3)
 generate_button = Button(text="Generate Password")
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=34)
+add_button = Button(text="Add", width=34, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
